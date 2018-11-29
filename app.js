88fda187cc1a6612a2e6f5e1fe1615a061e88c71
liubiao <liubiao@vlifepaper.com>
2018-11-29 17:04:08 +0800

    删除app.js无用的代码

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 import 'libs/adapter';
 
 App({
-    onLaunch: function () {
-        console.log('App Launch')
-    },
-    onShow: function () {
-        console.log('App Show')
-    },
-    onHide: function () {
-        console.log('App Hide')
+    onLaunch() {
     },
     globalData: {}
 });
