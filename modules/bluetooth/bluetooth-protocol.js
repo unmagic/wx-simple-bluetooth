59a273e05eb6e6929a5a625cc76f26674bc03f23
liubiao <liubiao@vlifepaper.com>
2018-12-03 15:32:27 +0800

    新增蓝牙未知状态和时间戳状态

--- modules/bluetooth/bluetooth-protocol.js
+++ modules/bluetooth/bluetooth-protocol.js
@@ -2,19 +2,22 @@ const commandIndex = 4, dataStartIndex = 5;
 
 const deviceIndexNum = 6;
 export default class BlueToothProtocol {
+    static TIMESTAMP = 'timestamp';//开始预热状态
     static PRE_HOT_START = 'pre_hot_start';//开始预热状态
     static PRE_HOT_FINISH_AND_START_BREATH = 'pre_hot_finish_and_start_breath';//预热完成开始吹气
     static BREATH_FINISH_AND_SUCCESS = 'breath_finish_and_success';//吹气完成返回结果
     static BREATH_RESTART = 'breath_restart';//重新吹气
     static DEVICE_ID_REQUIRE = 'device_id_require';//app请求设备串号
     static DEVICE_ID_GET_SUCCESS = 'device_id_get_success';//设备成功返回串号
+    static UNKNOWN = 'unknown';//未知状态
+
     constructor(blueToothManager) {
         this.action = {
             //由设备发出的时间戳请求
             '0x70': () => {
                 const now = Date.now() / 1000;
                 blueToothManager.sendData({buffer: this.createBuffer({command: '0x71', data: now})});
-                return {state: BlueToothProtocol.PRE_HOT_START};
+                return {state: BlueToothProtocol.TIMESTAMP};
             },
             //由设备发出的预热请求
             '0x72': () => {
@@ -62,19 +65,25 @@ export default class BlueToothProtocol {
             const endIndex = dataStartIndex + dataLength;
             dataArray = receiveArray.slice(dataStartIndex, endIndex + 1);
         }
-        console.log('16进制的命令字', commandHex, command, receiveArray);
-        return this.action[commandHex]({dataArray});
+        console.log('16进制的命令字', commandHex, receiveArray);
+        const action = this.action[commandHex];
+        if (action) {
+            return action({dataArray});
+        } else {
+            return {state: BlueToothProtocol.UNKNOWN};
+        }
     }
 
 
-    createBuffer({command, data = 0}) {
+    createBuffer({command, data}) {
         const dataBody = this.createDataBody({command, data});
         return new Uint8Array(dataBody).buffer;
     }
 
-    createDataBody({command = '', data = 0}) {
+    createDataBody({command = '', data}) {
         const dataPart = BlueToothProtocol.numToHexArray(data);
-        const lowLength = BlueToothProtocol.hexToNum(dataPart.length.toString(16)) + 2;
+        const lowLength = BlueToothProtocol.hexToNum((dataPart.length + 2).toString(16));
+        console.log(BlueToothProtocol.hexToNum(command), command, '发送的命令字');
         const array = [170, 0, lowLength, deviceIndexNum, BlueToothProtocol.hexToNum(command), ...dataPart];
         let count = 0;
         array.forEach(item => count += item);
@@ -84,6 +93,9 @@ export default class BlueToothProtocol {
     }
 
     static hexToNum(str = '') {
+        if (str.indexOf('0x') === 0) {
+            str = str.slice(2);
+        }
         return parseInt(`0x${str}`);
     }
 
@@ -91,12 +103,16 @@ export default class BlueToothProtocol {
         return ('00' + num.toString(16)).slice(-2);
     }
 
-    static numToHexArray(num = 0) {
+    static numToHexArray(num) {
+        if (num === void 0) {
+            return [];
+        }
+        num = parseInt(num);
         if (num === 0) {
             return [0];
         }
         let str = num.toString(16);
-        console.log(str);
+        console.log('num转为hex', str);
         str.length % 2 && (str = '0' + str);
         const array = [];
         for (let i = 0, len = str.length; i < len; i += 2) {
@@ -105,4 +121,3 @@ export default class BlueToothProtocol {
         return array;
     }
 }
-
