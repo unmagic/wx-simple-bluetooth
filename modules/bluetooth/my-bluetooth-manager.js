d552399047474997220cbfc7c88aaaab7fcc24f3
liubiao <liubiao@vlifepaper.com>
2018-12-04 15:56:55 +0800

    接收数据时更新状态

--- modules/bluetooth/my-bluetooth-manager.js
+++ modules/bluetooth/my-bluetooth-manager.js
@@ -63,7 +63,7 @@ export default class MyBlueToothManager extends SimpleBlueToothImp {
      */
     dealReceiveData({receiveBuffer}) {
         const {dataAfterProtocol, state} = this.bluetoothProtocol.receive({receiveBuffer});
-        // super.updateBLEStateImmediately({state});
+        super.updateBLEStateImmediately({state});
         // MyBlueToothManager.logReceiveData({receiveBuffer});
         let finalResult = dataAfterProtocol;
         //这里的result已经是拥有了总和及数据长度的一个ArrayBuffer了，这里应该是返回与UI层的渲染相关的数据，所以我这里是一个错误的演示
